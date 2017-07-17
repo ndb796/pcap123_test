f90fa32376ed35aee459eacf5ab80bbb6a07fbff
ndb796 <ndb796@naver.com>
2017-07-17 22:19:29 +0900

    Add Ethernet and IP structure

--- pcaptest.c
+++ pcaptest.c
@@ -15,6 +15,34 @@ struct pcap_pkthdr *header; // 패킷 관련 정보
 const u_char *packet; // 실제 패킷
 struct in_addr addr; // 주소 정보
 
+#define ETHER_ADDR_LEN 6
+
+struct sniff_ethernet {
+        u_char ether_dhost[ETHER_ADDR_LEN]; // 목적지 MAC 주소
+        u_char ether_shost[ETHER_ADDR_LEN]; // 출발지 MAC 주소
+        u_short ether_type;
+};
+
+#define IP_HL(ip) (((ip)->ip_vhl) & 0x0f)
+#define IP_V(ip) (((ip)->ip_vhl) >> 4)
+
+struct sniff_ip {
+        u_char ip_vhl;
+        u_char ip_tos;
+        u_short ip_len;
+        u_short ip_id;
+        u_short ip_off;
+        #define IP_RF 0x8000
+        #define IP_DF 0x4000
+        #define IP_MF 0x2000
+        #define IP_OFFMASK 0x1fff
+        u_char ip_ttl;
+        u_char ip_p; // IP 프로토콜 유형
+        u_short ip_sum;
+        struct in_addr ip_src; // 출발지 IP 주소
+        struct in_addr ip_dst; // 목적지 IP 주소
+};
+
 int main(void) {
 	return 0;
 }
